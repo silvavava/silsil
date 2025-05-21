bf4e553e2d33da284d424549a63563a354c4c677
silvavava <silvatrihandayani564@gmail.com>
2025-05-21 14:23:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("silvavava")
 st.write(
     "Let's start building!"
 )
-st.image("b707e7b4287edacce50e757116f0b2bb.jpg", width=200)
+st.image( "b707e7b4287edacce50e757116f0b2bb.jpg", width=200)
