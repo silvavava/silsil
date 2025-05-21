22e0351d91d743436aad4ba6f1e5fce623019a93
silvavava <silvatrihandayani564@gmail.com>
2025-05-21 13:49:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("selamar datang di web silvavava")
+st.title("selamat datang di web silvavava")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
